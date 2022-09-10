ba314c407b7ad54bdc957ff866d522bbcc83660a
salieri <shimahfy@gmail.com>
2022-09-10 18:19:46 +0800

    增加了寻找波峰波谷的函数，但是需要把数组改成二维同时存储index和波峰波谷值

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import phaseDiff
+# import calC
+
 def print_tdms():
     phaseDiff.getPhaseDiff()
 
 
 if __name__ == '__main__':
+    # l = 0.02761
+    # l = 0.05389
+    # f = 10000
+    # calC.calculate(l, f)
     print_tdms()
-
--- phaseDiff.py
+++ phaseDiff.py
@@ -5,12 +5,56 @@
 # @Software: PyCharm
 # @Others  :
 
-import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 from readsquidFiles import readsquidfiles
 
-def getPhaseDiff():
 
+def getPhaseDiff():
     # 读取tdsm文件
     DataMat, Fs, FileName = readsquidfiles()
+    # print(DataMat.size)
 
-    print(DataMat.loc[0])
+    window_size = 1000
+
+    # 选取通道
+    channel_code_1 = 0
+    channel_code_2 = 7
+    channel_name_1 = '/\'Magnetic\'/\'通道' + str(channel_code_1) + '\''
+    channel_name_2 = '/\'Magnetic\'/\'通道' + str(channel_code_2) + '\''
+
+    channel_1 = DataMat[channel_name_1]
+    channel_2 = DataMat[channel_name_2]
+
+
+
+# 寻找周期性信号的峰值与谷值
+def findMaxArray(channel):
+    channel_max = np.zeros()
+    channel_min = np.zeros()
+    index_max = np.zeros()
+    index_min = np.zeros()
+    print(channel.size)
+    temp_max = - 999999
+    temp_min = 999999
+    for i in range(0, channel.size - 3):
+        if channel[i] > channel[i + 1] & channel[i] > channel[i + 2] & channel[i] > channel[i + 3] & channel[i] > temp_max:
+            if temp_max == -999999:
+                channel_max = np.array(channel_max, channel[i])
+                index_max = np.array(index_max, i)
+                temp_min = 999999
+                temp_max = channel_max
+
+        if channel[i] < channel[i + 1] & channel[i] < channel[i + 2] & channel[i] < channel[i + 3] & channel[i] < temp_min:
+            if temp_min == 999999:
+                channel_min = np.array(channel_min, channel[i])
+                index_min = np.array(index_min, i)
+                temp_max = - 999999
+                temp_min = channel_min
+
+    # 补0使长度匹配
+    if channel_max.size > channel_min.size:
+        channel_min = np.pad(channel_min, np.zeros(channel_max.size - channel_min.size + 1))
+    elif channel_max.size < channel_min.size:
+        channel_max = np.pad(channel_max, np.zeros(channel_min.size - channel_max.size + 1))
+    return channel_max
